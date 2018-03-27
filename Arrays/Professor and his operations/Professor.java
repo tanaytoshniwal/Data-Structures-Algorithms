4584e1f5f9b9449aefe40262a17a52ea29b7f4b5
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2018-03-28 02:23:14 +0530

    updated MyScanner

--- Arrays/Professor and his operations/Professor.java
+++ Arrays/Professor and his operations/Professor.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 class TestClass{
   public static int[] swap(int[] arr, int x){
     int t=arr[x];
@@ -7,7 +8,7 @@ class TestClass{
     return arr;
   }
   public static void main(String[] args) throws Exeption{
-    Scanner sc=new Scanner(System.in);
+    MyScanner sc=new MyScanner();
     int n=sc.nextInt();
     int[] arr=new int[n];
     for(int i=0;i<n;i++)
@@ -22,5 +23,34 @@ class TestClass{
     }
     for(int i=0;i<n;i++)
       System.out.print(arr[i]+" ");
+  }
+}
+public static class MyScanner {
+  BufferedReader br;
+	StringTokenizer st;
+	public MyScanner() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+	}
+	String next() {
+	  while (st == null || !st.hasMoreElements()) {
+	    try {
+	      st = new StringTokenizer(br.readLine());
+	    } catch (IOException e) {
+	      e.printStackTrace();
+	    }
+	  }
+	  return st.nextToken();
+	}
+	int nextInt() {
+	  return Integer.parseInt(next());
+	}
+  String nextLine(){
+	  String str = "";
+		try {
+		  str = br.readLine();
+		} catch (IOException e) {
+		  e.printStackTrace();
+		}
+		return str;
   }
 }
