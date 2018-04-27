7688d4c62c140d644f2f3fb04c894fe500c61694
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2018-04-27 12:44:29 +0530

    HackerRank challenge

---
+++ Competitions-Challenges/HackerRank/Heaps/OperationsInHeap.java
@@ -0,0 +1,145 @@
+import java.io.*;
+import java.util.*;
+class MinHeap {
+    private int capacity;
+    private int size = 0;
+    private int[] elements;
+
+    public MinHeap(int capacity){
+        this.capacity = capacity;
+        elements = new int[capacity];
+    }
+
+    private int getLeftChildIndex(int parentIndex){
+        return 2*parentIndex + 1;
+    }
+    private int getRightChildIndex(int parentIndex){
+        return 2*parentIndex + 2;
+    }
+    private int getParentIndex(int childIndex){
+        return (childIndex-1)/2;
+    }
+    private int getIndex(int item){
+        int index=-1;
+        for(int i=0;i<size;i++){
+            if(elements[i] == item){
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public int capacity(){
+        return capacity;
+    }
+
+    private boolean hasLeftChild(int parentIndex){
+        return getLeftChildIndex(parentIndex) < size;
+    }
+    private boolean hasRightChild(int parentIndex){
+        return getRightChildIndex(parentIndex) < size;
+    }
+    private boolean hasParent(int childIndex){
+        return getParentIndex(childIndex) >= 0;
+    }
+
+    private int leftChild(int parentIndex){
+        return elements[getLeftChildIndex(parentIndex)];
+    }
+    private int rightChild(int parentIndex){
+        return elements[getRightChildIndex(parentIndex)];
+    }
+    private int parent(int childIndex){
+        return elements[getParentIndex(childIndex)];
+    }
+
+    private void swap(int first, int second){
+        int temp = elements[first];
+        elements[first] = elements[second];
+        elements[second] = temp;
+    }
+
+    private void ensureCapacity(){
+        if(size == capacity){
+            elements = Arrays.copyOf(elements, capacity*2);
+            capacity *= 2;
+        }
+    }
+
+    public int peek(){
+        if(size == 0)
+            throw new IllegalStateException();
+        return elements[0];
+    }
+
+    public int poll(){
+        if(size == 0)
+            throw new IllegalStateException();
+        int item = elements[0];
+        elements[0] = elements[size-1];
+        size--;
+        heapifyDown();
+        return item;
+    }
+
+    public void add(int item){
+        ensureCapacity();
+        elements[size] = item;
+        size++;
+        heapifyUp();
+    }
+
+    public void delete(int item){
+        if(size == 0)
+            throw new IllegalStateException();
+        int index = getIndex(item);
+        if(index == -1)
+            throw new IllegalStateException();
+        elements[index] = elements[size-1];
+        size--;
+        heapifyDown();
+    }
+
+    public void heapifyUp(){
+        int index = size-1;
+        while(hasParent(index) && (parent(index) > elements[index])){
+            swap(getParentIndex(index), index);
+            index = getParentIndex(index);
+        }
+    }
+
+    public void heapifyDown(){
+        int index = 0;
+        while(hasLeftChild(index)){
+            int smallerChildIndex = getLeftChildIndex(index);
+            if(hasRightChild(index) && leftChild(index) > rightChild(index))
+                smallerChildIndex = getRightChildIndex(index);
+            if(elements[index] < elements[smallerChildIndex])
+                break;
+            else
+                swap(index, smallerChildIndex);
+            index = smallerChildIndex;
+        }
+    }
+}
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        MinHeap heap = new MinHeap(n);
+        while(n-->0){
+            int a = sc.nextInt();
+            if(a==1){
+                heap.add(sc.nextInt());
+            }
+            if(a==2){
+                int t=sc.nextInt();
+                heap.delete(t);
+            }
+            if(a==3){
+                System.out.println(heap.peek());
+            }
+        }
+    }
+}
