3385c6880f773d978adc8db6192c0cb9c774d8f2
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2018-04-30 15:42:10 +0530

    heap sort

---
+++ Algorithms/Sorting/HeapSort/HeapSort.java
@@ -0,0 +1,160 @@
+import java.util.*;
+class MaxHeap {
+    private int capacity;
+    private int size = 0;
+    private int[] elements;
+
+    public MaxHeap(int capacity){
+        this.capacity = capacity;
+        elements = new int[capacity];
+    }
+
+    private int getLeftChildIndex(int parentIndex){
+        return 2*parentIndex + 1;
+    }
+    private int getRightChildIndex(int parentIndex){
+        return 2*parentIndex + 2;
+    }
+    private int getParentIndex(int childIndex){
+        return (childIndex-1)/2;
+    }
+    private int getIndex(int item){
+        int index=-1;
+        for(int i=0;i<size;i++){
+            if(elements[i] == item){
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public int capacity(){
+        return capacity;
+    }
+
+    private boolean hasLeftChild(int parentIndex){
+        return getLeftChildIndex(parentIndex) < size;
+    }
+    private boolean hasRightChild(int parentIndex){
+        return getRightChildIndex(parentIndex) < size;
+    }
+    private boolean hasParent(int childIndex){
+        return getParentIndex(childIndex) >= 0;
+    }
+
+    private int leftChild(int parentIndex){
+        return elements[getLeftChildIndex(parentIndex)];
+    }
+    private int rightChild(int parentIndex){
+        return elements[getRightChildIndex(parentIndex)];
+    }
+    private int parent(int childIndex){
+        return elements[getParentIndex(childIndex)];
+    }
+
+    private void swap(int first, int second){
+        int temp = elements[first];
+        elements[first] = elements[second];
+        elements[second] = temp;
+    }
+
+    private void ensureCapacity(){
+        if(size == capacity){
+            elements = Arrays.copyOf(elements, capacity*2);
+            capacity *= 2;
+        }
+    }
+
+    public int peek(){
+        if(size == 0)
+            throw new IllegalStateException();
+        return elements[0];
+    }
+
+    public int poll(){
+        if(size == 0)
+            throw new IllegalStateException();
+        int item = elements[0];
+        elements[0] = elements[size-1];
+        size--;
+        heapifyDown();
+        return item;
+    }
+
+    public void add(int item){
+        ensureCapacity();
+        elements[size] = item;
+        size++;
+        heapifyUp();
+    }
+
+    public void delete(int item){
+        if(size == 0)
+            throw new IllegalStateException();
+        int index = getIndex(item);
+        if(index == -1)
+            throw new IllegalStateException();
+        elements[index] = elements[size-1];
+        size--;
+        heapifyDown();
+    }
+
+    public void heapifyUp(){
+        int index = size-1;
+        while(hasParent(index) && (parent(index) < elements[index])){
+            swap(getParentIndex(index), index);
+            index = getParentIndex(index);
+        }
+    }
+
+    public void heapifyDown(){
+        int index = 0;
+        while(hasLeftChild(index)){
+            int largerChildIndex = getLeftChildIndex(index);
+            if(hasRightChild(index) && leftChild(index) < rightChild(index))
+                largerChildIndex = getRightChildIndex(index);
+            if(elements[index] > elements[largerChildIndex])
+                break;
+            else
+                swap(index, largerChildIndex);
+            index = largerChildIndex;
+        }
+    }
+
+    public void sort(){
+        while(size>1){
+            int root = peek();
+            elements[0] = elements[size-1];
+            elements[size-1] = root;
+            size--;
+            heapifyDown();
+        }
+    }
+
+    public void print(){
+        for(int i=0;i<elements.length;i++)
+            System.out.print(elements[i]+" ");
+    }
+}
+class TestClass {
+    public static void main(String[] args){
+        System.out.print("Input size of max-heap:");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        try{
+            if(n<0)
+                throw new IllegalStateException("Invalid size");
+        }
+        catch(IllegalStateException e){
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        MaxHeap heap = new MaxHeap(n);
+        System.out.println("Input "+n+" elements:");
+        while(n-->0)
+            heap.add(sc.nextInt());
+        heap.sort();
+        heap.print();
+    }
+}
