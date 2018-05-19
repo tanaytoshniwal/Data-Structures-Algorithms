51b9fddf3dd939e7a98157970ec4acdf7a21b20a
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2018-05-20 02:18:08 +0530

    linked list

--- DataStructures-Implementation/LinkedLists/LinkedList.java
+++ DataStructures-Implementation/LinkedLists/LinkedList.java
@@ -1,47 +1,77 @@
-import java.util.Scanner;
-class LinkedList{
-	static Node start=null;
-	static class Node{
-		int data;
-		Node node;
-		Node(int data){
-			this.data=data;
-			this.node=null;
-		}
-		boolean hasNext(){
-			return (this.node!=null);
-		}
-	}
-	static void printList(){
-		Node print=start;
-		System.out.println("LinkedList is :");
-		while(print.node!=null){
-			System.out.print(print.data+" ");
-			print=print.node;
-		}
-		System.out.println(print.data);
-	}
-	public static void main(String[] args){
-		LinkedList list=new LinkedList();
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Enter 5 elements to add:");
-		start=new Node(scan.nextInt());
-		Node n1=new Node(scan.nextInt());start.node=n1;
-		Node n2=new Node(scan.nextInt());n1.node=n2;
-		Node n3=new Node(scan.nextInt());n2.node=n3;
-		Node n4=new Node(scan.nextInt());n3.node=n4;
-		n4.node=null;
-		System.out.print("Printing LinkedList");
-		for(int i=0;i<3;i++){
-			try{
-				Thread.sleep(500);
-			}catch(InterruptedException e){
-				e.printStackTrace();
-			}
-			System.out.print(".");
-		}
-		System.out.println();
-		printList();
-		System.out.println("end of program");
-	}
+import java.util.*;
+class EmptyListException extends Exception{
+    public EmptyListException(String str){
+        super(str);
+    }
+}
+class LinkedList {
+    static Node currentNode=null;
+    static Node startingNode=null;
+    static class Node{
+        Node previous = null;
+        Node next = null;
+        int data;
+        Node(){
+            this.previous=null;
+            this.next=null;
+        }
+        Node(int data){
+            this.data = data;
+        }
+        boolean hasNext(){
+            return this.next!=null;
+        }
+        boolean hasPrevious(){
+            return this.previous!=null;
+        }
+        void next(Node next){
+            this.next = next;
+        }
+        void previous(Node previous){
+            this.previous = previous;
+        }
+        int data(){
+            return this.data;
+        }
+        void put(int data){
+            this.data = data;
+        }
+    }
+    LinkedList(){
+        Node node = new Node();
+        currentNode = node;
+        startingNode = currentNode;
+    }
+    void add(int element){
+        Node node = new Node(element);
+        currentNode.next(node);
+        node.previous(currentNode);
+        currentNode = node;
+    }
+    void print() throws Exception{
+        if(startingNode == null){
+            throw new EmptyListException("oops its empty");
+        }
+        Node iterator = startingNode;
+        while(iterator.hasNext()){
+            iterator = iterator.next;
+            System.out.print(iterator.data()+" ");
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.print("Input size of the linked list:");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        LinkedList list = new LinkedList();
+        for(int i=0;i<n;i++)
+            list.add(sc.nextInt());
+
+        System.out.println("Your list is:");
+        try{
+            list.print();
+        }
+        catch(EmptyListException e){
+            e.printStackTrace();
+        }
+    }
 }
