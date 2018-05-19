af2aee3f30e7723e9f0513da39b55d6521ae6905
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2018-05-19 17:52:46 +0530

    counting sort testing

--- Algorithms/Sorting/CountingSort/CountingSort.java
+++ Algorithms/Sorting/CountingSort/CountingSort.java
@@ -1,30 +1,48 @@
 import java.util.*;
+import java.io.*;
 class CountingSort {
+    /*void sort(char arr){
+        int n = arr.length;
+        char output[] = new char[n];
+        int count[] = new int[256];
+        for (int i=0; i<256; ++i)
+            count[i] = 0;
+
+        for (int i=0; i<n; ++i)
+            ++count[arr[i]];
+        for (int i=1; i<=255; ++i)
+            count[i] += count[i-1];
+        for (int i = 0; i<n; ++i){
+            output[count[arr[i]]-1] = arr[i];
+            --count[arr[i]];
+        }
+        for (int i = 0; i<n; ++i)
+            arr[i] = output[i];
+    }*/
     public static void main(String[] args) throws Exception {
         System.out.print("Input size of the array:");
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        char[] input = new char[n];
+        char[] output = new char[n];
+        int[] chars = new int[256];
+        Arrays.fill(chars, 0);
         System.out.println("Input "+n+" elements:");
-        int[] arr=new int[n];
-        HashMap<Integer, Integer> count = new HashMap<>();
-        for(int i=0;i<10;i++)
-            count.put(i, 0);
-        for(int i=0;i<n;i++) {
-            arr[i]=sc.nextInt();
-            count.put(arr[i], count.get(arr[i])+1);
+        for(int i=0;i<n;i++){
+            input[i] = sc.next().charAt(0);
+            ++chars[input[i]];
+            System.out.println(chars[input[i]]+"::::::"+input[i]+":::"+i);
         }
-        Arrays.fill(arr, -1);
-        for(int a:count.keySet()){
-            if(a!=0)
-                count.put(a, count.get(a)+count.get(a-1));
-            int t=count.get(a);
-            while(t-->0){
-                if(arr[t]==-1)
-                    arr[t]=a;
-            }
+        for(int i=1;i<256;i++){
+            chars[i]=chars[i]+chars[i+1];
+            if(chars[i]!=0)
+            System.out.println(i+":::"+chars[i]);
         }
-        System.out.println("Sorted Array:");
+        for (int i = 0; i<n; i++){
+            output[chars[input[i]]-1] = input[i];
+            --chars[input[i]];
+        }
         for(int i=0;i<n;i++)
-            System.out.print(arr[i]+" ");
+            System.out.print(output[i]+" ");
     }
 }
