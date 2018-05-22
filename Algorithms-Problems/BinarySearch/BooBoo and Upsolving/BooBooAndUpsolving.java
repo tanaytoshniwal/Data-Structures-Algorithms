d51101f8af4a15ead71bef8c83a847c3a9143562
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2018-05-22 17:16:42 +0530

    booboo and upsolving

---
+++ Algorithms-Problems/BinarySearch/BooBoo and Upsolving/BooBooAndUpsolving.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class TestClass {
+    public static boolean check(long[] arr, int m, long mid){
+        long c=0, days=1;
+        for(int i=0;i<arr.length;){
+            if(arr[i]>mid)
+                return false;
+            if(arr[i]+c<=mid){
+                c+=arr[i++];
+            }
+            else{
+                c=0;days++;
+            }
+        }
+        return m>=days;
+    }
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        long[] arr=new long[n];
+        long sum=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextLong();
+            sum+=arr[i];
+        }
+        long l=0, h=sum;
+        while(l<h){
+            long mid = (l+h)/2;
+            if(check(arr, m, mid))
+                h=mid;
+            else
+                l=mid+1;
+        }
+        System.out.println(l);
+    }
+}
