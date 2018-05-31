0f9d1eb3ebd1bba8d721bf33a9632a894d7d0df0
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2018-05-31 17:44:57 +0530

    post order tree traversal

---
+++ DataStructures-Implementation/TreeTraversal/PostOrderTraversal/PostOrderTraversal.java
@@ -0,0 +1,43 @@
+class Node {
+    int data;
+    Node leftChild;
+    Node rightChild;
+    public Node(int data){
+        this.data = data;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+    public int getData(){
+        return this.data;
+    }
+}
+class BinaryTree {
+    Node rootNode;
+    public BinaryTree(){
+        this.rootNode = null;
+    }
+    public BinaryTree(int data){
+        this.rootNode = new Node(data);
+    }
+}
+class TestClass {
+    public static void postOrderPrint(Node node){
+        if(node == null)
+            return;
+        postOrderPrint(node.leftChild);
+        postOrderPrint(node.rightChild);
+        System.out.print(node.getData()+" ");
+    }
+    public static void main(String[] args){
+        BinaryTree tree = new BinaryTree();
+        tree.rootNode = new Node(1);
+        tree.rootNode.leftChild = new Node(2);
+        tree.rootNode.rightChild = new Node(3);
+        tree.rootNode.leftChild.leftChild = new Node(4);
+        tree.rootNode.leftChild.rightChild = new Node(5);
+        tree.rootNode.rightChild.leftChild = new Node(6);
+        tree.rootNode.rightChild.rightChild = new Node(7);
+
+        postOrderPrint(tree.rootNode);
+    }
+}
