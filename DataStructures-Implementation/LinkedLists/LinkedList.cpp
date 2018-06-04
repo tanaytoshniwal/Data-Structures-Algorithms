f0192ed8a3d78f3be2b988d782dda16dc022ba58
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2018-06-05 01:48:00 +0530

    linked list update

--- DataStructures-Implementation/LinkedLists/LinkedList.cpp
+++ DataStructures-Implementation/LinkedLists/LinkedList.cpp
@@ -11,9 +11,9 @@ class LinkedList {
         head = NULL;
         tail = NULL;
     }
-	void add(int value) {
+	void add(int data) {
         Node *temp = new Node;
-        temp->data = value;
+        temp->data = data;
         temp->next = NULL;
         if(head == NULL) {
             head = tail = temp;
@@ -23,13 +23,61 @@ class LinkedList {
             tail = temp;
         }
 	}
-    void print(){
+    void add(int data, int index) {
+        Node *pre = new Node;
+        Node *cur = new Node;
+        Node *temp = new Node;
+        temp->data = data;
+        cur = head;
+        for(int i=0;i<index+1;i++){
+            pre = cur;
+            cur = cur->next;
+        }
+        pre->next = temp;
+        temp->next = cur;
+    }
+    void addFirst(int data) {
+        Node *temp = new Node;
+        temp->data = data;
+        temp->next = head;
+        head = temp;
+    }
+    void remove(){
+        Node *pre = new Node;
+        Node *cur = new Node;
+        cur = head;
+        while(cur->next != NULL){
+            pre = cur;
+            cur = cur->next;
+        }
+        pre->next =NULL;
+        delete cur;
+    }
+    void remove(int index) {
+        Node *pre = new Node;
+        Node *cur = new Node;
+        cur = head;
+        for (int i = 0; i < index+1; i++) {
+            pre = cur;
+            cur = cur->next;
+        }
+        pre->next = cur->next;
+        delete cur;
+    }
+    void removeFirst(){
+        Node *first = new Node;
+        first = head;
+        head = head->next;
+        delete first;
+    }
+    void print() {
         Node *temp = new Node;
         temp = head;
         while(temp!=NULL){
             cout<<temp->data<<" ";
             temp = temp->next;
         }
+        cout<<endl;
     }
 
 };
@@ -43,5 +91,14 @@ int main(){
         cin>>t;
         list.add(t);
     }
+    list.print();
+    list.add(10);
+    list.addFirst(10);
+    list.print();
+    list.add(6,6);
+    list.print();
+    list.remove(6);
+    list.remove();
+    list.removeFirst();
     list.print();
 }
