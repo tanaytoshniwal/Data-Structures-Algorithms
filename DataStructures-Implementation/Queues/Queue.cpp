e178ba90650880f6a2f94fb0940dcdcdab1c3d09
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2018-08-11 17:13:30 +0530

    queue

--- DataStructures-Implementation/Queues/Queue.cpp
+++ DataStructures-Implementation/Queues/Queue.cpp
@@ -7,13 +7,39 @@ struct Node {
 
 class Queue{
     public:
-    Node* head, tail;
+    Node *head, *tail;
     Queue(){
-
+        head = NULL;
+        tail = NULL;
+    }
+    void add(int data){
+        cout<<"add";
+        Node* temp = new Node();
+        temp->data = data;
+        if(head == NULL){
+            head = temp;
+        }
+        else{
+            tail = temp;
+            tail = tail->next;
+        }
+    }
+    void print(){
+        cout<<"print";
+        Node *temp = head;
+        while(temp != NULL){
+            cout<<temp->data<<endl;
+            temp = temp->next;
+        }
     }
 };
 
 int main(){
     Queue q;
+    q.add(1);
+    q.add(1);
+    q.add(1);
+    q.add(1);
+    q.print();
     return 0;
 }
