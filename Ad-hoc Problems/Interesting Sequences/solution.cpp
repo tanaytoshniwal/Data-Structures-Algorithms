bc86d49b800e8e162b0ace2819197bebec5bf1bc
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2019-06-07 23:15:54 +0530

    interesting sequences

--- Ad-hoc Problems/Interesting Sequences/solution.cpp
+++ Ad-hoc Problems/Interesting Sequences/solution.cpp
@@ -1,26 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
-    int n, k, l;
+    int n,k,l;
     cin >> n >> k >> l;
-    int* arr = new int[n];
+    int *arr = new int[n];
     for(int i=0;i<n;i++) cin >> arr[i];
     sort(arr, arr+n);
-    int low = arr[0];
-    int high = arr[n-1];
-    int mincost = INT_MAX;
-    for(int pivot = low;pivot<=high;pivot++){
+    int min_cost = INT_MAX;
+    for(int i=arr[0];i<=arr[n-1];i++){
         int inc = 0, dec = 0;
-        for(int i=0;i<n;i++){
-            if(arr[i]<pivot) inc += pivot - arr[i];
-            else dec += arr[i] - pivot;
+        for(int j=0;j<n;j++){
+            if(i>arr[j]) inc+=i-arr[j];
+            else dec+=arr[j]-i;
         }
-        if(inc >= dec){
+        if(inc>=dec){
             int cost = (dec*k) + ((inc-dec)*l);
-            if(cost < mincost) mincost = cost;
+            if(cost < min_cost) min_cost = cost;
         }
     }
-    cout << mincost << endl;
-    delete[] arr;
+    cout << min_cost << endl;
     return 0;
 }
