2c085f8a0babaa930dcdb1753862de2fe62522e7
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2019-07-14 00:57:44 +0530

    delete from start

---
+++ Data Structures/DataStructures-Problems/LinkedLists/Delete From Start/solution.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef struct Node Node;
+struct Node{
+    int data;
+    Node* next;
+};
+Node* insert(Node* head, int data){
+    if(head == NULL){
+        head = new Node;
+        head->data = data;
+        head->next = NULL;
+        return head;
+    }
+    Node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    Node* node = new Node;
+    node->data = data;
+    temp->next = node;
+    return head;
+}
+void print(Node* head){
+    if(head == NULL) return;
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+Node* remove(Node* head){
+    if(head == NULL) return NULL;
+    Node* rem = head;
+    head = head->next;
+    delete rem;
+    return head;
+}
+int main(){
+    Node* head = NULL;
+    int n;
+    cin >> n;
+    while(n--){
+        int t;
+        cin >> t;
+        head = insert(head, t);
+    }
+    head = remove(head);
+    print(head);
+    return 0;
+}
