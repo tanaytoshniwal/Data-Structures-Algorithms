3ce9849657a52de7dab1a7b67944a6ea01be14d3
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2019-08-28 06:48:01 +0530

    BFS Graphs

---
+++ Data Structures/Graphs/bfs_adjacency_matrix.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bfs_print(int** graph, int V, int sv, int* visited){
+    queue<int> q;
+    q.push(sv);
+    visited[sv] = 1;
+    while(!q.empty()){
+        int t = q.front();
+        q.pop();
+        cout << t << " ";
+        for(int i=0;i<V;i++){
+            if(i == t) continue;
+            if(graph[t][i] && !visited[i]){
+                visited[i] = 1;
+                q.push(i);
+            }
+        }
+    }
+}
+
+int main() {
+    int V, E;
+    cin >> V >> E;
+    int** graph = new int*[V];
+    for(int i=0;i<V;i++){
+        graph[i] = new int[V];
+        for(int j=0;j<V;j++) graph[i][j] = 0;
+    }
+    int* visited = new int[V];
+    for(int i=0;i<V;i++) visited[i] = 0;
+    for(int i=0;i<E;i++){
+        int a, b;
+        cin >> a >> b;
+        graph[a][b] = graph[b][a] = 1;
+    }
+    
+    bfs_print(graph, V, 0, visited);
+    return 0;
+}
