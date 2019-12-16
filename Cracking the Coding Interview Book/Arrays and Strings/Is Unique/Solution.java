bf815236ef48274b7a87b5a4cbf96e2459865aac
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2019-12-17 00:51:16 +0530

    cracking the coding interview book - initial

---
+++ Cracking the Coding Interview Book/Arrays and Strings/Is Unique/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Solution{
+    static String isUnique(String str){
+        
+    }
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println("Is Unique:" + isUnique(input));
+    }
+}
