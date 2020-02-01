75bf26a2c5b2b926fd6da56f9639e53aebf3a848
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2020-02-02 03:58:12 +0530

    string rotation solution

---
+++ Cracking the Coding Interview Book/Arrays and Strings/String Rotation/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isSubstr(string s1, string s2){
+    return s1.find(s2) != string::npos;
+}
+bool isRotation(string s1, string s2){
+    s1 += s1;
+    return isSubstr(s1, s2);
+}
+int main(){
+    string s1, s2;
+    cin >> s1 >> s2;
+    cout << isRotation(s1, s2);
+    return 0;
+}
