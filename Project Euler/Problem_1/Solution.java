09889c84ff8e5e202cef607a7f818914031d34ba
Eric Park <me@ericswpark.com>
2021-10-03 00:47:55 +0900

    Solve Project Euler/Problem 1 - Java

---
+++ Project Euler/Problem_1/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 0; i < 1000; i++) {
+			if (i % 3 == 0 || i % 5 == 0)
+				sum += i;
+		}
+		System.out.println(sum);
+    }
+}
