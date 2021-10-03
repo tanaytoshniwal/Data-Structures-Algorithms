b7b9310c82fae5ab3a543094bfaa49dcb26538aa
nkhandelwal07 <91761839+nkhandelwal07@users.noreply.github.com>
2021-10-03 10:51:03 -0700

    requested check.py changes
    I used the and operator instead of mod.

--- Algorithms/Bit Manipulation/Check Odd or Even/check.py
+++ Algorithms/Bit Manipulation/Check Odd or Even/check.py
@@ -1,6 +1,8 @@
 #Checks for Odd or Even in Python
 def OddOrEven(n):
-  if n % 2 == 0
+  binary_value = n & 1 #even if 0 and odd if 1
+  if binary_value == 0:
     return "Even"
   else:
     return "Odd"
+  
