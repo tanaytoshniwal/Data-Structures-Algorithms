f5706b43820f9c9308ac63c18ebb642c0a370d84
Nidita <64700758+Nidita@users.noreply.github.com>
2021-10-05 11:29:10 +0600

    Problem_1 c++ implementation

---
+++ Project Euler/Problem_1/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int sum=0;
+    for (int i=1;i<1000;i++)
+    {
+        if(i%3==0 || i%5==0)
+        {
+            sum+=i;
+
+        }
+    }
+    cout<<sum<<endl;
+}
